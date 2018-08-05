d20c8495200db5da4180bb4f91a2c556285929eb
roryjackd <roryjackd@yahoo.com>
2018-08-05 09:24:16 -0600

    Created posts with property & event binding

--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -1,4 +1,4 @@
-import { Component} from '@angular/core';
+import { Component, Input} from '@angular/core';
 
 @Component({
   selector: 'app-post-list',
@@ -11,6 +11,6 @@ export class PostListComponent {
   //   {title: 'Second Post', content: 'This is the first post\'s content'},
   //   {title: 'Third Post', content: 'This is the first post\'s content'},
   // ];
-  posts = [];
+  @Input() posts = [];
 }
 
