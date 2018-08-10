b857f11c28be4aabeda279601f5d3e179938f0f3
roryjackd <roryjackd@yahoo.com>
2018-08-10 13:40:19 -0600

    connected to mongoose

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,18 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+
+const Post = require('./models/post');
 
 const app = express();
+
+mongoose.connect("mongodb+srv://rory:pmRoJAXVqbpQys3J@cluster0-5u963.mongodb.net/test?retryWrites=true")
+    .then(()=>{
+        console.log('Connected to datebase.')
+    })
+    .catch(() => {
+        console.log('Connection failed.');
+    });
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -20,7 +31,10 @@ app.use((req, res, next) => {
 });
 
 app.post("/api/posts", (req, res, next) => {
-  const post = req.body;
+  const post = new Post({
+      title: req.body.title,
+      content: req.body.content
+  });
   console.log(post);
   res.status(201).json({
     message: 'Post added successfully'
