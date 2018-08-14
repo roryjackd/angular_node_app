9f8f93f133fd379d7e189c7c770a6a7ae1c5637b
roryjackd <roryjackd@yahoo.com>
2018-08-14 15:08:06 -0600

    Added pagination component

--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit, OnDestroy } from "@angular/core";
-import { Subscription } from 'rxjs';
+import { PageEvent } from "@angular/material";
+import { Subscription } from "rxjs";
 
 import { Post } from "../post.model";
-import { PostsService } from "../posts.service";
+import { PostsService } from "../posts.service";;
 
 @Component({
   selector: "app-post-list",
@@ -17,6 +18,10 @@ export class PostListComponent implements OnInit, OnDestroy {
   // ];
   posts: Post[] = [];
   isLoading = false;
+  totalPosts = 0;
+  postsPerPage = 2;
+  currentPage = 1;
+  pageSizeOptions = [1, 2, 5, 10];
   private postsSub: Subscription;
 
   constructor(public postsService: PostsService) {}
@@ -29,6 +34,13 @@ export class PostListComponent implements OnInit, OnDestroy {
         this.isLoading = false;
         this.posts = posts;
       });
+  }
+
+  onChangedPage(pageData: PageEvent) {
+    this.isLoading = true;
+    this.currentPage = pageData.pageIndex + 1;
+    this.postsPerPage = pageData.pageSize;
+    this.postsService.getPosts(this.postsPerPage, this.currentPage);
   }
 
   onDelete(postId: string) {
